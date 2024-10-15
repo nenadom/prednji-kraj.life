fe5650731682d1fd8220dcfd09d64bd1010e00e9
Nenad Mijatovic <me@nenad.io>
2024-10-15 11:21:20 +0200

    Github pages

---
+++ precache-manifest.745fa8db4593498fd8deedad694c183a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e957d8fbaf5e4ecff539a8a8f7fe0bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12dd1016e5bf83b8db2",
+    "url": "/static/css/main.b01ad1fb.chunk.css"
+  },
+  {
+    "revision": "44b079fcb3517e08d21d",
+    "url": "/static/js/2.e2699680.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.e2699680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12dd1016e5bf83b8db2",
+    "url": "/static/js/main.bfb9bee0.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
